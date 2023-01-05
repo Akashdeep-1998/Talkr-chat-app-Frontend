b9f9e802690782b654eeb5f5f2ba04e4df158e45
Akash <akashdeep.lpu@outlook.com>
2023-01-05 13:30:21 +0530

    app finished

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import chatIcon from "./chatIcon2.png";
+
+import "./App.css";
+import Chat from "./components/Chat/Chat";
+
+const socket = io("https://talkr.vercel.app/");
+
+const App = () => {
+  const [username, setUserName] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [roomJoined, setRoomJoined] = useState(false);
+
+  const joinRoom = () => {
+    socket.emit("join_room", { username: username, roomId: roomId });
+    setRoomJoined(true);
+  };
+
+  useEffect(() => {
+    socket.emit("user_joined", {
+      username: username,
+      roomId: roomId,
+    });
+  }, [socket, roomJoined]);
+
+  return (
+    <div className="App">
+      {!roomJoined ? (
+        <div className="login-window">
+          <div className="login-header">
+            <img src={chatIcon} height="45" alt="" />
+            <h2>Talkr - Join Room</h2>
+          </div>
+          <div className="input-box">
+            <input
+              placeholder="Enter your name"
+              name="username"
+              onChange={(e) => setUserName(e.target.value.trim())}
+            />
+            <input
+              placeholder="Enter Room ID"
+              name="roomId"
+              onChange={(e) => setRoomId(e.target.value.trim())}
+            />
+
+            <button
+              className={username !== "" && roomId !== "" ? "" : "disabled"}
+              disabled={!username || !roomId}
+              onClick={joinRoom}
+            >
+              <h3>Join Room</h3>
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Chat username={username} roomId={roomId} socket={socket} />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import uuid from "react-uuid";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+import sendButton from "../../sendButton.png";
+import chatIcon from "../../chatIcon2.png";
+
+import "./Chat.css";
+
+const Chat = (props) => {
+  const [message, setMessage] = useState({});
+  const [messageList, setMessageList] = useState([
+    {
+      username: props.username,
+      text: `Welcome ${props.username}!`,
+      time: new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }),
+      newJoined: true,
+    },
+  ]);
+
+  const onChangeHandler = (e) => {
+    setMessage({
+      username: props.username,
+      roomId: props.roomId,
+      text: e.target.value,
+      time: new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }),
+    });
+  };
+
+  const sendMessage = async () => {
+    if (message.text !== "") {
+      await props.socket.emit("send_message", message);
+      setMessageList([...messageList, message]);
+    }
+
+    setMessage({ text: "" });
+  };
+
+  useEffect(() => {
+    props.socket.on("new_user_joined", (userData) => {
+      setMessageList([
+        ...messageList,
+        {
+          text: `${userData.username} joined the room!`,
+          username: userData.username,
+          time: new Date().toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          }),
+          newJoined: true,
+        },
+      ]);
+    });
+
+    props.socket.on("receive_message", (messageData) => {
+      setMessageList([...messageList, messageData.message]);
+    });
+
+    props.socket.on("left", (user) => {
+      setMessageList([
+        ...messageList,
+        {
+          text: `${user.username} left the room.`,
+          time: new Date().toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          }),
+          userLeft: true,
+        },
+      ]);
+    });
+  }, [props.socket, messageList]);
+
+  return (
+    <>
+      <div className="chat-window">
+        <div className="chat-header">
+          <img src={chatIcon} height="45" alt="" />
+          <h2>Talkr</h2>
+          <h4>
+            Hi {props.username.split(" ")[0]}
+            <span style={{ color: "#31e331" }}>&nbsp;&#x2022;</span>
+          </h4>
+        </div>
+        <div className="message-body">
+          <ScrollToBottom className="scrollBottom">
+            {messageList.map((messageData) => (
+              <div
+                key={uuid()}
+                className={`message ${
+                  props.username === messageData.username ? "right" : "left"
+                }`}
+              >
+                {!messageData.newJoined && !messageData.userLeft ? (
+                  <b>
+                    {props.username === messageData.username
+                      ? "You"
+                      : messageData.username}
+                    :&nbsp;
+                  </b>
+                ) : null}
+                <p className="message-para">{messageData.text}</p>
+                <small>
+                  <blockquote>{messageData.time}</blockquote>
+                </small>
+              </div>
+            ))}
+          </ScrollToBottom>
+        </div>
+      </div>
+      <div className="message-footer">
+        <input
+          placeholder="Write a message..."
+          value={message.text}
+          onChange={onChangeHandler}
+          onKeyPress={(e) => e.key === "Enter" && sendMessage()}
+        />
+        <img src={sendButton} height="40" alt="" onClick={sendMessage} />
+      </div>
+    </>
+  );
+};
+
+export default Chat;
